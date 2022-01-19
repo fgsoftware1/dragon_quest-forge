b0bc11929570e8c3403797aafc8582d183cb469a
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2022-01-19 12:48:37 +0000

    setting up main mod class
    Signed-off-by: Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>

--- src/main/java/com/fgsoftwarestudio/dragonquest/DragonquestMod.java
+++ src/main/java/com/fgsoftwarestudio/dragonquest/DragonquestMod.java
@@ -1,4 +1,31 @@
 package com.fgsoftwarestudio.dragonquest;
 
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("dragonquest")
 public class DragonquestMod {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public DragonquestMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::ClientRegistries);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        LOGGER.info("SETUP");
+    }
+
+    private void ClientRegistries(final FMLClientSetupEvent event) {
+        LOGGER.info("REGISTRY");
+    }
 }
